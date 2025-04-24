5a5141ba611ca20b1df4f149dd68f54e58c5dccb
sabri4525 <sabrinavicente4525@gmail.com>
2025-04-24 20:59:47 -0300

    Elimino archivos que no deberían estar en esta rama

--- README.md
+++
@@ -1,1 +0,0 @@
-# Tecnicas-de-Compilacion
--- compilador-proyecto/src/main/antlr4/com/compilador/App.java
+++
@@ -1,8 +0,0 @@
-package com.ejemplo.compilador;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Bienvenido al compilador!");
-        // Aquí se puede iniciar el proceso de análisis utilizando ANTLR
-    }
-}
